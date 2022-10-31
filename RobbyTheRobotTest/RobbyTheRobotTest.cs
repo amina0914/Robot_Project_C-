52c86b685e3a9f741e2501fb6de666a828733a74
amina.turd <aminaturdalieva@gmail.com>
2022-10-30 21:01:33 -0400

    added first silly testing method just to see if it works

---
+++ RobbyTheRobotTest/RobbyTheRobotTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RobbyTheRobotTest
+{
+    [TestClass]
+    public class RobbyTheRobotTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int number = 4;
+            Assert.AreEqual(4,number);
+        }
+    }
+}
