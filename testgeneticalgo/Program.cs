3099ec3b30ce7abd42d23bf2a67a55972396ee6b
Octavio <gancoc1@hotmail.com>
2022-11-03 22:50:41 -0400

    Creation of test branch

---
+++ testgeneticalgo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testgeneticalgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
